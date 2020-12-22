8e35fbaa1efd441576958543b84f13364bc669e2
alex he <hebailanc@outlook.com>
2020-12-22 14:58:36 +0100

    update ignore and add initial files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-setup(name='Package Name',
-      version='0.0.1',
-      description='BR AI and Automation Lab: Satellite ImageryDatei',
-      author='Bailan He',
-      author_email='Bailan.He@campus.lmu.de',
-      packages=['Package_File'],
-      install_requires=['matplotlib','numpy==1.15.4', 'pycodestyle>=2.4.0'])
