607764e6bab21b7959dab3f3c4507e7753cc7a7f
alex he <hebailanc@outlook.com>
2021-01-27 11:15:29 +0100

    add new plot function and add new io pipeline for sentine-2 images.

--- functions/io_pipe.py
+++ functions/io_pipe.py
@@ -94,13 +94,84 @@ def get_landsat8(aoi=None,
             'bbox': _roi_bbox,
             'time_interval': time_interval
         },
-        # save: {'eopatch_folder': './eopatch'}
+        # save: {'eopatch_folder': './landsat_eopatch'}
     })
     eopatch = _result.eopatch()
 
     return eopatch
 
 
-if __name__ == '__main__':
-    set_config()
-    eo_test = get_landsat8()
+def get_sentinel2(aoi=None,
+                 time_interval=('2020-04-01', '2020-05-01'),
+                 maxcc=.8,
+                 resolution=20,
+                 config=None):
+    """
+    Download sentinel2 images from sentinelhub.
+    Args:
+        param aoi: Area of Interest.
+        type aoi: shapely.geometry.multipolygon.MultiPolygon.
+        
+        param time_interval: Time period want to get.
+        type time_interval: two elements list. (Start, End).
+        
+        param maxcc: Maximum cloud coverage.
+        type maxcc: float.
+        
+        param resolution: The resolution of images
+        type resolution: int.
+        
+        param config: 
+        type config: sentinelhub.config.SHConfig
+        
+    Returns:
+    A eopatch datatable, which has the image data, mask data.
+    image data: np.ndarray, shape(time,image_width,image_height, bands)
+    mask data: np.ndarray, shape(time,image_width,image_height, 1)
+    """
+    if aoi == None:
+        # if aoi not specificed use munich as default
+        _munich = gpd.read_file('../geojson/munich.geojson')
+        _interested_area = _munich.geometry.unary_union
+        _bbox_interested_area = _interested_area.bounds
+        _roi_bbox = BBox(bbox=_bbox_interested_area, crs=CRS.WGS84)
+    else:
+        _roi_bbox = aoi.bounds
+    if not config:
+        config_dict = {
+            'INSTANCE_ID': '31aacbb6-8ad8-43f5-b19d-84a8302c2a3e',
+            'CLIENT_ID': '8c799cf6-53fa-4f98-a3ff-417cdc658b57',
+            'CLIENT_SECRET': '13[,;5upS5%e3@oZk?J^1:)Fu?*.+0kF|,kD6re2'
+        }
+        config = set_config(config_dict)
+
+    _time_difference = datetime.timedelta(hours=2)
+
+    input_task = SentinelHubInputTask(
+        data_collection=DataCollection.SENTINEL2_L1C,
+        bands=['B01','B02','B03','B04','B05','B06','B07','B08','B8A','B09','B10','B11','B12'],
+        bands_feature=(FeatureType.DATA, 'L1C_data'),
+        additional_data=[(FeatureType.MASK, 'dataMask')],
+        resolution=resolution,
+        maxcc=maxcc,
+        time_difference=_time_difference,
+        config=config,
+        max_threads=3)
+    save = SaveTask('data_sentinel', overwrite_permission=2, compress_level=1)
+    workflow = LinearWorkflow(input_task)
+
+    _result = workflow.execute({
+        input_task: {
+            'bbox': _roi_bbox,
+            'time_interval': time_interval
+        },
+        # save: {'eopatch_folder': './sentinel_eopatch'}
+    })
+    eopatch = _result.eopatch()
+
+    return eopatch
+
+
+# if __name__ == '__main__':
+#     set_config()
+#     eo_test = get_landsat8()
