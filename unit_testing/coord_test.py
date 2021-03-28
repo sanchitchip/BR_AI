5b7029f534d09ba9eed94a22dbf805d7f0b20b11
Sanchit <chiplunkarsanchit1@gmail.com>
2021-03-28 18:21:34 +0200

    added test cases for get_coord functions.

---
+++ unit_testing/coord_test.py
@@ -0,0 +1,76 @@
+import geopandas as gpd
+import sys
+sys.path.insert(1,"../functions/")
+import pytest
+import io_pipe
+import nd_index
+from eolearn.core import EOPatch
+import numpy as np
+import pdb
+import lst
+import get_coord
+
+def test_coord_matrix_input_type():
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_coord_matrix("s",int(34),[35,45])
+
+def test_coord_matrix_input_type2():
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_coord_matrix(int(30),'s',[35,45])
+
+## checking if we get a None type error if we input a None value in the function.
+def test_geoindex_Noneerror():
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_geoindex(None,(24,56))
+    assert 'NoneType' in str(msg.value)
+
+def test_geoindex_Noneerror():
+    val = ((1,2,3),(4,67,8))
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_geoindex(val,None)
+    assert 'NoneType' in str(msg.value)
+
+
+## checking if we get a None type error if we input a None value in the function.
+def test_makebbox_Noneerror():
+    with pytest.raises(TypeError) as msg:
+        get_coord.make_bbox(None,(24,56))
+    assert 'NoneType' in str(msg.value)
+
+def test_makebbox_Noneerror():
+    val = ((1,2,3),(4,67,8))
+    with pytest.raises(TypeError) as msg:
+        get_coord.make_bbox(val,None)
+    assert 'NoneType' in str(msg.value)
+
+def test_makebbox_input_type():
+    with pytest.raises(TypeError) as msg:
+        get_coord.make_bbox(25,(24,56))
+    #assert 'Type' in str(msg.value)
+
+## checking if we get a None type error when we input a None value in the function for get bbox function.
+def test_getbbox_input_type():
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_bbox(2324,(21,516))
+    #assert 'Type' in str(msg.value)
+
+def test_getbbox_Noneerror():
+    val = ((1,2,3),(4,67,8))
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_bbox(None,val)
+    assert 'NoneType' in str(msg.value)
+
+def test_getebbox_Noneerror():
+    val = ((1,2,3),(4,67,8))
+    with pytest.raises(TypeError) as msg:
+        get_coord.get_bbox(val,None)
+    assert 'NoneType' in str(msg.value)
+
+
+
+#def test_testing():
+#    with pytest.raises(TypeError):
+        
+#    print(e_info.value)
+#    assert str(e_info.value) in 'division by zero'
+
